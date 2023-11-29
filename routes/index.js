eec9e37433f4f7a421620d8990f525c64f7134e2
yagyaev.s.i.22@gmail.com <yagyaev.s.i.22@gmail.com>
2023-11-29 10:00:22 +0300

    «Commit»

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ var router = express.Router();
 
 /* GET ice. */
 router.get('/ice_', function(req, res, next) {
-  res.send("<h1>Страница ice</h1>")
+  res.render('ice_', {
+    title: "Лед",
+    picture: "images/ice2.jpg",
+    desc: "Как мы видим на этой картинке лед."
+       });
   });
-
+  
   /* GET ice_cubes. */
 router.get('/ice_cubes', function(req, res, next) {
-  res.send("<h1>Страница ice cubes</h1>")
+  res.render('ice_', {
+    title: "Лед",
+    picture: "images/ice_.jpg",
+    desc: "Как мы видим на этой картинке лед, но в другой форме."
+       });
   });
 
   /* GET ice2. */
 router.get('/ice2', function(req, res, next) {
-  res.send("<h1>Страница ice2</h1>")
+  res.render('ice_', {
+    title: "Лед",
+    picture: "images/ice2.jpg",
+    desc: "Как мы видим на этой картинке лед."
+       });
   });
   
 
