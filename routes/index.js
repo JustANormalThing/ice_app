a67661dd1a8d4e66486a5fd72b0517ecec8017e9
yagyaev.s.i.22@gmail.com <yagyaev.s.i.22@gmail.com>
2023-11-29 09:37:37 +0300

    «Writting»

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET ice. */
+router.get('/ice_', function(req, res, next) {
+  res.send("<h1>Страница ice</h1>")
+  });
+
+  /* GET ice_cubes. */
+router.get('/ice_cubes', function(req, res, next) {
+  res.send("<h1>Страница ice cubes</h1>")
+  });
+
+  /* GET ice2. */
+router.get('/ice2', function(req, res, next) {
+  res.send("<h1>Страница ice2</h1>")
+  });
+  
 
 module.exports = router;
