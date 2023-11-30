ec78488aeeb20ddb1826d603687c97226f67b758
yagyaev.s.i.22@gmail.com <yagyaev.s.i.22@gmail.com>
2023-11-30 09:49:15 +0300

    «Working»

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = [{
+    title: "Лед",
+    nick: 'ice_',
+    avatar: '/images/ice2.jpg',
+    desc: 'Как мы видим на этой картинке лед..'
+    },
+    
+    {
+    title: 'Лед',
+    nick: 'ice_cubes',
+    avatar: '/images/ice_.png',
+    desc: 'Как мы видим на этой картинке лед..'
+    },
+    {
+    title: 'Лед',
+    nick: 'ice2',
+    avatar: '/images/ice2.png',
+    desc: 'Как мы видим на этой картинке лед..'
+    }
+    ];
+    module.exports.data = data;
