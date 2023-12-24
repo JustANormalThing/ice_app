2b4ea27818c867fd2a292224386b173d86dea7dd
yagyaev.s.i.22@gmail.com <yagyaev.s.i.22@gmail.com>
2023-12-24 17:50:02 +0300

    «We_Connected»

--- routes/ice2_.js
+++ routes/ice2_.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const Ice_ = require("../models/ice_").Ice_;
-var checkAuth = require("./../middleware/checkAuth.js")
+//const Ice_ = require("../models/ice_").Ice_;
+//var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
 
 const async = require("async");
 
-router.get('/:nick',checkAuth, async function(req, res, next) {
-  try {
-    const [ice_, ice2_] = await Promise.all([
-      Ice_.findOne({ nick: req.params.nick }),
-      Ice_.find({}, { _id: 0, title: 1, nick: 1 })
-    ]);
 
-    if (!ice_) {
-      throw new Error("Нет такого");
-    }
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM ices WHERE ices.nick = '${req.params.nick}'`, (err,ices) => {
+  if(err) {
+    console.log(err);
+    if(err) return next(err)
+    } else {
+      if(ices.length == 0) return next(new Error("Такого льда нету"))
+        var ice_ = ices[0];
+        res.render('ice_', {
+          title: ice_.title,
+          picture: ice_.avatar,
+          desc: ice_.about
+  })
+}
+})
 
-    res.render('ice_', {
-      title: ice_.title,
-      picture: ice_.avatar,
-      desc: ice_.desc,
-    });
-  } catch (err) {
-    next(err);
-  }
 });
+
+//router.get('/:nick',checkAuth, async function(req, res, next) {
+  //try {
+   // const [ice_, ice2_] = await Promise.all([
+   //   Ice_.findOne({ nick: req.params.nick }),
+   //   Ice_.find({}, { _id: 0, title: 1, nick: 1 })
+   // ]);
+
+  //  if (!ice_) {
+   //   throw new Error("Нет такого");
+   // }
+
+  //  res.render('ice_', {
+  //    title: ice_.title,
+  //    picture: ice_.avatar,
+  //    desc: ice_.desc,
+  //  });
+ // } catch (err) {
+ //   next(err);
+//  }
+//});
 
 module.exports = router;
--- seedMySQL.js
+++ seedMySQL.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql2');
 var drop = 'TRUNCATE TABLE ices;'
-var seedQuery = 'INSERT INTO ices (id,title, nick, avatar, about) VALUES (1,"Лед", "ice_", "/images/ice2.jpg", "Как мы видим на этой картинке лед.."),  (2,"Лед_Кубик", "ice_cubes", "/images/ice_.png", "Мы видим лед но уже в кубичейской форме"),  (3,"Лед2", "ice2_", "/images/ice2_.png", "Лед сделан из воды ну как бы");'
+var seedQuery = 'INSERT INTO ices (id,title, nick, avatar, about) VALUES (1,"Лед", "ice_", "/images/ice2.jpg", "Как мы видим на этой картинке лед.."),  (2,"Лед_Кубик", "ice_cubes", "/images/ice_.jpg", "Мы видим лед но уже в кубичейской форме"),  (3,"Лед2", "ice2", "/images/ice2.jpg", "Лед сделан из воды");'
 
 var connection = mysql.createConnection({
     host : '127.0.0.1',
