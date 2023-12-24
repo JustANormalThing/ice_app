0f68cc7e0d4814986d296bacc067840dee8d4fe0
yagyaev.s.i.22@gmail.com <yagyaev.s.i.22@gmail.com>
2023-12-24 17:53:11 +0300

    «menu»

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -3,7 +3,7 @@ const Ice_ = require("./../models/ice_").Ice_;
 module.exports = async function(req, res, next) {
   try {
     res.locals.nav = [];
-    const result = await Ice_.find({}, { _id: 0, title: 1, nick: 1 });
+    const result = await Ice_.find(null, { _id: 0, title: 1, nick: 1 });
     res.locals.nav = result;
     next();
   } catch (err) {
